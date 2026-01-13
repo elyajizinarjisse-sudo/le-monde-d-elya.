70af349fe4836c265e20d37fb24d8c494bf9a245
Narjisse I9 <sylvainrobert68hotmail.com@iMac-de-Narjisse.local>
2026-01-12 22:55:11 -0500

    Fix build: remove unused React import and add react-quill

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Hero } from '../home/Hero';
 import { ProductSection } from '../home/ProductSection';
 import { BlogSection } from '../home/BlogSection';
